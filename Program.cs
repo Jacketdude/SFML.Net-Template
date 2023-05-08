6369cd2ec9bb4a3c258756aade51c00eeed2799d
J Dev <dudleings@gmail.com>
2023-05-08 20:20:58 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using SFML;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace sfmlnet
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            const int WIDTH = 900;
+            const int HEIGHT = 600;
+
+            RenderWindow app = new RenderWindow(new VideoMode(WIDTH, HEIGHT), "SFML.Net Window");
+            app.Closed += (sender, e) => app.Close();
+
+            while (app.IsOpen)
+            {
+                app.DispatchEvents();
+                app.Clear(Color.Cyan);
+                app.Display();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Introduction
+This is just a template for the SFML .NET binder if you want to get a quick start on
+SFML.NET.
+
+## Version
+This is only a 64bit version, I'll make a 32bit version once it's done.
+
+## Install
+1. Clone this repository.
+2. Cd into the directory of this project.
+3. Run "dotnet run" to run the project
+    * For building of this project run "dotnet build".
+
+## Editing/Changing of files
+You can change stuff via the Program.cs file and you can add more scripts to this.
